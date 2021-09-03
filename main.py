6b5e10082e978c15d3d6c5ad3331d23b58f06f89
RayaLitova <raya.litova23@gmail.com>
2021-09-03 23:16:57 +0300

    create map, add numbers, remove poss, print map

--- main.py
+++ main.py
@@ -1,3 +1,7 @@
+import math
+
+global map 
+
 class tile_t:
     def __init__ (self, num, poss, square_num, line, column):
         self.num = num
@@ -6,5 +10,54 @@ class tile_t:
         self.line = line
         self.column = column
 
-tile=tile_t(0,[1,2,3,4,5,6,7,8,9], 3, 4, 2)
-print(tile.num)
+def create_map():
+    global map
+    map=[]
+    for i in range(9):
+        map.append([])
+        for j in range(9):
+            temp=tile_t(0,[1,2,3,4,5,6,7,8,9],(math.floor(i/3)+2*(math.floor(i/3))+math.floor(j/3)),i,j)
+            map[i].append(temp)
+
+def add_nums():
+    global map
+    while(1):
+        line=int(input("Line: "))-1
+        
+        if line==-1: 
+            break
+
+        column=int(input("Column: "))-1
+        num=int(input("Number: "))
+
+        map[line][column].num = num 
+        remove_poss(line,column)
+
+def remove_poss(line,column):
+    global map
+    for i in range(9):
+        if map[line][column].num in  map[line][i].poss:
+            map[line][i].poss.remove(map[line][column].num)
+
+    for i in range(9):
+        if map[line][column].num in  map[i][column].poss:
+            map[i][column].poss.remove(map[line][column].num)
+
+    for i in range(3):
+        for j in range(3):
+            if map[line][column].num in map[i+math.floor(map[line][column].square_num/3)*3][j+((map[line][column].square_num - (math.floor(map[line][column].square_num/3)*3))*3)].poss:
+                map[i+math.floor(map[line][column].square_num/3)*3][j+((map[line][column].square_num - (math.floor(map[line][column].square_num/3)*3))*3)].poss.remove(map[line][column].num)
+
+def print_map():
+    global map
+    for i in range(9):
+        print("\n")
+        for j in range(9):
+            print(map[i][j].poss)
+
+def game():
+    create_map()
+    add_nums()
+    print_map()
+
+game()
